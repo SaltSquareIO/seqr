be86d5db889a3627448118f0e39741ef44c90658
Emir Mujic <28683200+emirkum@users.noreply.github.com>
2024-01-08 00:08:57 +0100

    Update migrations.sh

--- docker/migrations/migrations.sh
+++ docker/migrations/migrations.sh
@@ -1,4 +1,6 @@
-/bin/sh
+#!/usr/bin/env bash
+
+set -x -e
 
 python -m manage makemigrations 
 python -m manage migrate 
@@ -6,9 +8,12 @@ python -m manage loaddata variant_tag_types // This will fail if it has been run
 python -m manage loaddata variant_searches // This will fail if it has been run before, and that is okay
 python -m manage reload_saved_variant_json
 
-psql -U postgres postgres -c "drop database reference_data_db"
-psql -U postgres postgres -c "create database reference_data_db"
+PGPASSWORD="${POSTGRES_PASSWORD}"
+
+psql -h ${POSTGRES_SERVICE_HOSTNAME} -U postgres postgres -c "drop database reference_data_db"
+psql -h ${POSTGRES_SERVICE_HOSTNAME} -U postgres postgres -c "create database reference_data_db"
+
 REFERENCE_DATA_BACKUP_FILE=gene_reference_data_backup.gz
 wget -N https://storage.googleapis.com/seqr-reference-data/gene_reference_data_backup.gz -O ${REFERENCE_DATA_BACKUP_FILE}
-psql -U postgres reference_data_db <  <(gunzip -c ${REFERENCE_DATA_BACKUP_FILE})
+psql -h ${POSTGRES_SERVICE_HOSTNAME} -U postgres reference_data_db <  <(gunzip -c ${REFERENCE_DATA_BACKUP_FILE})
 rm ${REFERENCE_DATA_BACKUP_FILE}
