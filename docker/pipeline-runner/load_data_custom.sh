2d96361060569db3a60834a1d9635ff266925d86
Emir Mujic <28683200+emirkum@users.noreply.github.com>
2024-01-15 11:17:11 +0100

    Update load_data_custom.sh

--- docker/pipeline-runner/load_data_custom.sh
+++ docker/pipeline-runner/load_data_custom.sh
@@ -19,6 +19,14 @@ case ${BUILD_VERSION} in
     exit 1
 esac
 
+export HOST=$(curl -s 169.254.169.254/latest/meta-data/local-hostname)
+export LOCAL_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
+
+cat /etc/hosts
+echo `ec2-metadata -o|cut -d: -f2` " " `ec2-metadata -h |cut -d: -f2` >> /etc/hosts
+cat /etc/hosts
+echo $LOCAL_IP
+
 cd /
 mkdir -p /dataset
 mount-s3 test-seqr-bucket /dataset
