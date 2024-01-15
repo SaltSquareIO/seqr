f68b81b94577e0c819a34b14a2c36c9698bad859
Emir Mujic <28683200+emirkum@users.noreply.github.com>
2024-01-15 14:34:55 +0100

    Update load_data_custom.sh

--- docker/pipeline-runner/load_data_custom.sh
+++ docker/pipeline-runner/load_data_custom.sh
@@ -22,6 +22,9 @@ esac
 export HOST=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
 export LOCAL_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
 
+export SPARK_MASTER_HOST=localhost
+export SPARK_LOCAL_IP="127.0.0.1"
+
 echo "localhost $LOCAL_IP" >> /etc/hosts
 cat /etc/hosts
 
