bb1a16e744502d174acc4d220a948fbb1f890ba7
Emir Mujic <28683200+emirkum@users.noreply.github.com>
2024-01-15 18:33:13 +0100

    Update load_data_custom.sh

--- docker/pipeline-runner/load_data_custom.sh
+++ docker/pipeline-runner/load_data_custom.sh
@@ -77,3 +77,4 @@ python3 -m seqr_loading SeqrMTToESTask --local-scheduler \
     --dont-validate \
     --source-paths "${SOURCE_FILE}" \
     --dest-path "${DEST_FILE}"
+    --grch38-to-grch37-ref-chain "/dataset/grch38_to_grch37.over.chain.gz"
